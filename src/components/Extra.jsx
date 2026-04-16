a3bd4be8795348c0f02ff0f6d788ed2b872834cb
TedNedNo <nishantadhikari915@gmail.com>
2026-04-16 21:49:36 +0545

    Added Login

--- src/components/Extra.jsx
+++ src/components/Extra.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React,{useState} from 'react'
 import close from '../assets/close.png'
+import Login from './Login'
 const Extra = ({onClick}) => {
+    const [loginPage,setLoginPage]=useState(false);
+    const handleLogin = ()=>{
+        setLoginPage(prev=>!prev)
+    }
+    const removeLogin =()=>{
+        setLoginPage(false);
+    }
   return (
     <nav className='absolute top-0 right-0 w-screen h-screen bg-slate-100 text-gray-950'>
         <img src={close} 
@@ -9,11 +17,14 @@ const Extra = ({onClick}) => {
         onClick={onClick}
         />
       <ul className='flex flex-col list-none gap-3 mt-28 pl-10 text-3xl'>
-        <li className="border-2 border-amber-400 w-4/5 rounded-md text-center h-16 ">Login</li>
+        <li className="border-2 border-amber-400 w-4/5 rounded-md text-center h-16 "
+        onClick={handleLogin}
+        >Login</li>
         <li className="border-2 border-amber-400 w-4/5 rounded-md text-center h-16 ">Register</li>
         <li className="border-2 border-amber-400 w-4/5 rounded-md text-center h-16 ">About Us</li>
         <li className="border-2 border-amber-400 w-4/5 rounded-md text-center h-16 ">User Information</li>
       </ul>
+      {loginPage && <Login onClick={removeLogin}/>}
     </nav>
   )
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import close from '../assets/close.png'
+
+const Login = ({onClick}) => {
+    
+  return (
+    <div className="min-h-screen w-screen fixed top-0 left-0 bg-gray-50 flex items-center justify-center">
+        <img src={close} 
+                alt="Close" 
+                className="absolute top-2 left-0 w-10 h-10"
+                onClick={onClick}
+        />
+  <form className="w-full max-w-sm p-5 bg-white rounded-xl shadow space-y-4">
+    
+    <h1 className="text-xl font-semibold text-gray-900">Register</h1>
+
+    <div>
+      <label className="text-sm text-gray-700 mb-1 block">Full Name</label>
+      <input type="text" className="w-full h-12 px-4 text-base border border-gray-500 rounded-lg focus:ring-2 focus:ring-blue-500 outline-none" />
+    </div>
+
+    <div>
+      <label className="text-sm text-gray-700 mb-1 block">Email</label>
+      <input type="email" className="w-full h-12 px-4 text-base border border-gray-500 rounded-lg focus:ring-2 focus:ring-blue-500 outline-none" />
+    </div>
+
+   <div>
+      <label className="text-sm text-gray-700 mb-1 block">Password</label>
+      <input type="password" className="w-full h-12 px-4 text-base border border-gray-500 rounded-lg focus:ring-2 focus:ring-blue-500 outline-none" />
+    </div>
+
+    <button className="w-full h-12 bg-blue-500 text-white text-base font-medium rounded-lg active:scale-95 transition">
+      Submit
+    </button>
+
+  </form>
+</div>
+  )
+}
+
+export default Login
