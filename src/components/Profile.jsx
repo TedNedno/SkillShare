e3c6ea1729f1d6e2260df34c2e8de066d75ae9dd
TedNedNo <nishantadhikari915@gmail.com>
2026-04-16 20:02:19 +0545

    Add image

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import boy from "../assets/boy.png"
+import girl from "../assets/girl.png"
 
 import { useContext } from 'react';
 import { InformationContext } from '../contexts/informationContext';
@@ -8,7 +10,7 @@ const Profile = ({sid}) => {
     const user= data.find(student => student.sid==sid);
   return (
     <section className="w-full h-2/5 flex bg-slate-900 text-green-600 ">
-      <p className='text-[16rem] mx-auto mb-10 [text-shadow:0_0_5px_#facc15,0_0_5px_#eab308,0_0_5px_#ca8a04]'>{user.icon}</p>
+      <img className='text-[16rem] mx-auto mb-10 [text-shadow:0_0_5px_#facc15,0_0_5px_#eab308,0_0_5px_#ca8a04]' src={user.icon=="M"?boy:girl} />
     </section>
   )
 }
